c80b97ff8e14b6b893391f8968b821b7bf966f4b
daichi.sugiyama <caad12.di2@gmail.com>
2024-03-15 22:50:31 +0900

    テーブル生成マイグレーションファイルを追加 #3

---
+++ database/migrations/2024_03_15_211202_create_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->ulid('item_id')->primary();
+            $table->string('item_name', 26);
+            $table->string('status')->default('stock');
+            $table->char('user_id', 26)->nullable();
+            $table->dateTime('purchased_at')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
