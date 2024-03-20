a93be4e691c93ff81aeacc35e3230be2d503b091
daichi.sugiyama <caad12.di2@gmail.com>
2024-03-20 12:46:57 +0900

    初期デザインMoc実装 #7

---
+++ resources/js/Layouts/Header.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Box, Flex, Text, Link, Icon, Badge } from '@chakra-ui/react';
+import { FiShoppingCart } from 'react-icons/fi';
+import { FaUser } from 'react-icons/fa';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string;
+}
+
+interface TopBannerProps {
+  cartItemCount: number;
+  auth: {
+    user: User;
+  };
+}
+
+const TopBanner: React.FC<TopBannerProps> = ({ cartItemCount, auth }) => {
+  return (
+    <Box w="100%" bg="blue.600" color="white" px={4} py={2}>
+      <Flex align="center" justify="space-between">
+        <Link href="/" _hover={{ textDecoration: 'none' }}>
+          <Text fontSize="xl" fontWeight="bold">
+            みかたんにってぃんぐ
+          </Text>
+        </Link>
+        <Flex>
+          {auth.user ? (
+            <Link
+              href={route('dashboard')}
+              className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400"
+              p={2}
+              _hover={{ textDecoration: 'none' }}
+            >
+              <Icon as={FaUser} w={6} h={6} />
+            </Link>
+          ) : (
+            <>
+              <Link
+                href={route('login')}
+                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400"
+                p={2}
+                _hover={{ textDecoration: 'none' }}
+              >
+                ログイン
+              </Link>
+
+              <Link
+                href={route('register')}
+                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400"
+                p={2}
+                _hover={{ textDecoration: 'none' }}
+              >
+                会員登録
+              </Link>
+            </>
+          )}
+          <Link
+            href="/cart"
+            position="relative"
+            p={2}
+            _hover={{ textDecoration: 'none' }}
+          >
+            <Icon as={FiShoppingCart} w={6} h={6} />
+            {cartItemCount > 0 && (
+              <Badge
+                colorScheme="red"
+                ml="-1"
+                mt="-1"
+                position="absolute"
+                top="0"
+                right="0"
+              >
+                {cartItemCount}
+              </Badge>
+            )}
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default TopBanner;
--- resources/js/Pages/TopPage.tsx
+++ resources/js/Pages/TopPage.tsx
@@ -1,7 +1,8 @@
-import { Link, Head } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
 import IndexCard from '@/Components/Items/IndexCard';
 import { Grid, GridItem } from '@chakra-ui/react';
+import Header from '@/Layouts/Header';
 
 export default function Welcome({
   auth,
@@ -9,34 +10,10 @@ export default function Welcome({
   return (
     <>
       <Head title="Welcome" />
+      <div>
+        <Header cartItemCount={2} auth={auth} />
+      </div>
       <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen">
-        <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-          {auth.user ? (
-            <Link
-              href={route('dashboard')}
-              className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400"
-            >
-              Dashboard
-            </Link>
-          ) : (
-            <>
-              <Link
-                href={route('login')}
-                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400"
-              >
-                Log in
-              </Link>
-
-              <Link
-                href={route('register')}
-                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400"
-              >
-                Register
-              </Link>
-            </>
-          )}
-        </div>
-
         <div className="max-w-7xl mx-auto p-6 lg:p-8">
           <Grid templateColumns="repeat(2, 1fr)" gap={6} px={5} py={5}>
             {[1, 2, 3, 4, 5, 6].map(() => (
